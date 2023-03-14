dae43e6a77c2c80029660b9c6fb7ab1581272e41
Astik Singh <assi@odoo.com>
2023-03-14 14:53:15 +0530

    fetching data and next and previous button

--- Activity 2/script/script.js
+++ Activity 2/script/script.js
@@ -1,29 +1,28 @@
 let div = document.createElement('div');
+div.setAttribute("id",'set');
 
-
-        // div.style.height= "500px";
-        // div.style.width= "250px";
-        // div.style.border= "thick solid black";
-
-
-// document.getElementById("set").style.border = "";
+div.setAttribute()
 
 function display(data){
     for(i=0; i<10; i++){
-        // div.style.height= "500px";
-        // div.style.width= "250px";
-        
-        console.log('hii')
-        
+        div.innerHTML += `<img src="${data[i].image}" alt="${data[i].title}" width="100px" height="100px">
+        <h3>${data[i].title}</h3>
+        <p>Price: ${data[i].price}</p>`
+
+        document.getElementById('main').appendChild(div);
+    }
+}
+
+function display2(data){
+    for(i=10; i<20; i++){
         div.innerHTML += `<img src="${data[i].image}" alt="${data[i].title}" width="100px" height="100px">
         <h3>${data[i].title}</h3>
         <p>Price: ${data[i].price}</p>;`
 
         document.getElementById('main').appendChild(div);
-
-        
     }
 }
+
 
 async function Demo(){
     const response = await fetch('https://fakestoreapi.com/products');
@@ -38,3 +37,44 @@ Demo().then((result)=>{
 }) 
 
 
+
+
+
+
+
+function Next(){
+
+    document.getElementById('set').innerHTML="";
+    document.getElementById('prev').setAttribute("class","d-inline");
+    document.getElementById('prev').setAttribute("class","btn");
+    async function Demo(){
+        const response = await fetch('https://fakestoreapi.com/products');
+        const json = await response.json();
+        // console.log(JSON.stringify(json));
+        return json;
+    }
+
+    Demo().then((result)=>{
+        console.log(result[0]);
+        display2(result)
+    }) 
+
+}
+
+function Prev(){
+    document.getElementById('prev').setAttribute("class","d-none")
+
+    async function Demo(){
+        const response = await fetch('https://fakestoreapi.com/products');
+        const json = await response.json();
+        // console.log(JSON.stringify(json));
+        return json;
+    }
+    
+    Demo().then((result)=>{
+        console.log(result[0]);
+        display(result)
+    }) 
+   
+
+}

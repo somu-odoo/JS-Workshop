ce2c53622e8e0971298ee0d2d73b819dbb7b5fa7
Mayurrajsinh Rathod <mrsr@odoo.com>
2023-03-14 16:08:17 +0530

    Task#1 OWL and Guess game with owl

--- Task:2/script.js
+++ Task:2/script.js
@@ -1,5 +1,9 @@
 // let url="https://fakestoreapi.com/products";
-
+let c=10;
+let i=0;
+// let btn=document.getElementById("next")
+// let count=
+const contain=document.createElement('div');
 async function demo(){
     const x = await fetch("https://fakestoreapi.com/products")
     const data= await x.json()
@@ -9,11 +13,9 @@ async function demo(){
 }
 
 demo()
-.then((result)=>{
-    let i;
-    for(i=0;i<10;i++){
+.then((result,i)=>{
+    for(i=0;i<c;i++){
         // console.log(result[i])
-        const contain=document.createElement('div');
         contain.classList.add('pro')
         contain.innerHTML += `<img src="${result[i]['image']}" width="300px" height="400px">
         <h3>${result[i].title}</h3>
@@ -23,3 +25,24 @@ demo()
     }
 }
 )
+next.addEventListener('click',()=>{
+    console.log("Hello")
+    contain.innerHTML="";
+    i=10
+    c=20
+    demo()
+    .then((result)=>{
+        for(i=i;i<c;i++){
+            // console.log(result[i])
+            const contain=document.createElement('div');
+            contain.classList.add('pro')
+            contain.innerHTML += `<img src="${result[i]['image']}" width="300px" height="400px">
+            <h3>${result[i].title}</h3>
+            <p>${result[i].description}</p>
+            <h3>&#8377;${result[i].price}</h3>`;
+            document.getElementById("container1").appendChild(contain);
+        }
+    }
+)
+
+})
